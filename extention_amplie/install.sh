d7bb744a0a627a87c14c6c8587fbea6b1f42648b
quentin guiheneuc <quentin.guiheneuc@ynov.com>
2020-04-21 20:11:28 +0200

    v1

--- extention_amplie/install.sh
+++ extention_amplie/install.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
-$bassemoode = /var/www/
-$moodedb = /var/local/www/db/
+
 #----------------------------
+sudo mkdir script /var/www/
 sudo apt-get install i2c-tools
 sudo apt-get install python-smbus
 sudo pip install pyserial
-sudo mv index1.php $bassemoode
-sudo mv parametre_control.php $bassemoode
+sudo mv index1.php /var/www/
+sudo mv parametre_control.php /var/www/
 sudo mv header.php /var/local/www/
-sudo mv index.js $bassemoode/js/
-sudo mkdir script $bassemoode
-sudo mv serial1.py $bassemoode/script/
+sudo mv index.js /var/www/js/
+sudo mv serial1.py /var/www/script/
+sudo mv cfg-BTN-sqlite3.db /var/local/www/db/
 sudo chown 0:50 cfg-BTN-sqlite3.db
 sudo chomod 777 cfg-BTN-sqlite3.db
-sudo mv cfg-BTN-sqlite3.db $moodedb 
 
 
