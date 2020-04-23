5667e822de44f63c8fc4c1c0213266d43775bfda
quentin guiheneuc <quentin.guiheneuc@ynov.com>
2020-04-23 15:38:18 +0200

    v1 install

--- extention_amplie/install.sh
+++ extention_amplie/install.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
 
 #----------------------------
-sudo mkdir script /var/www/
-sudo apt-get install i2c-tools
-sudo apt-get install python-smbus
-sudo pip install pyserial
-sudo mv index1.php /var/www/
-sudo mv parametre_control.php /var/www/
-sudo mv header.php /var/local/www/
-sudo mv index.js /var/www/js/
-sudo mv serial1.py /var/www/script/
-sudo mv cfg-BTN-sqlite3.db /var/local/www/db/
-sudo chown 0:50 cfg-BTN-sqlite3.db
-sudo chomod 777 cfg-BTN-sqlite3.db
+
+apt-get install i2c-tools
+apt-get install python-smbus
+pip install pyserial
+cp index1.php /var/www/
+cp parametre_control.php /var/www/
+cp header.php /var/local/www/
+cp index.js /var/www/js/
+mkdir script 
+cp serial1.py /script/
+cp I2c.py /script/
+mv script /var/www/
+cp cfg-BTN-sqlite3.db /var/local/www/db/
+chown 0:50 cfg-BTN-sqlite3.db
+chomod 777 cfg-BTN-sqlite3.db
 
 
