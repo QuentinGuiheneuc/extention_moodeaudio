0416fdd7620241384f9da6013565303770402768
quentin guiheneuc <quentin.guiheneuc@ynov.com>
2020-04-29 19:30:49 +0200

    v2

--- extention_amplie/6.4.2/index.js
+++ extention_amplie/6.4.2/index.js
@@ -1,26 +1,24 @@
-var time = true
+var time = true;
 function ajouter() {
-    document.getElementById("Ajout").className = "col-container-fluid"
-    document.getElementById("Supp").className = "container-fluid hide"
-    document.getElementById("Modi").className = "container-fluid hide"
-
+  document.getElementById("Ajout").className = "col-container-fluid";
+  document.getElementById("Supp").className = "container-fluid hide";
+  document.getElementById("Modi").className = "container-fluid hide";
 }
 function supp() {
-    document.getElementById("Ajout").className = "container-fluid hide"
-    document.getElementById("Supp").className = "container-fluid"
-    document.getElementById("Modi").className = "container-fluid hide"
-
+  document.getElementById("Ajout").className = "container-fluid hide";
+  document.getElementById("Supp").className = "container-fluid";
+  document.getElementById("Modi").className = "container-fluid hide";
 }
 function modif() {
-    document.getElementById("Ajout").className = "container-fluid hide"
-    document.getElementById("Supp").className = "container-fluid  hide"
-    document.getElementById("Modi").className = "container-fluid"
-
+  document.getElementById("Ajout").className = "container-fluid hide";
+  document.getElementById("Supp").className = "container-fluid  hide";
+  document.getElementById("Modi").className = "container-fluid";
+}
+function fermer() {
+  document.getElementById("Ajout").className = "container-fluid hide";
+  document.getElementById("Supp").className = "container-fluid  hide";
+  document.getElementById("Modi").className = "container-fluid hide";
 }
 function setnc(params) {
-    consol.log(params);
+  consol.log(params);
 }
-
-
-
-
